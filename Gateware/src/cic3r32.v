24fcb0bf6f3025f547e89a779187b8e594633335
cgbarn35 <cgbarn35@gmail.com>
2025-07-02 15:30:39 -0700

    barebones test environment, PDM CIC filtering

---
+++ Gateware/src/cic3r32.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+
+module CICNR16 #(parameter N=3)( //16 bit output 
+	input clk,
+	input clkdiv,
+	input rst,
+	input x_in,
+	output[15:0] y_out
+);
+
+reg x;
+reg [25:0] I [N-1:0];//Integrators
+reg [25:0] C [N  :0];//Combs
+reg [25:0] CD[N-1:0];//Comb Delays
+
+//INTEGRATORS
+
+wire [25:0] Itemp;
+assign Itemp = I[0];//VISUALIZATION FOR GTKWAVE
+
+always @(posedge clk or posedge rst)
+begin
+	if(rst) begin 
+		I[0][25:0] <= 0;
+	end else begin 
+		I[0][25:0] <= I[0][25:0] + x_in;
+	end
+end
+
+//COMBS
+
+wire [25:0] Ctemp;
+wire [25:0] CDtemp;
+assign Ctemp = C[0];//VISUALIZATION FOR GTKWAVE
+assign CDtemp = CD[0];//VISUALIZATION FOR GTKWAVE
+
+always @(posedge clkdiv or posedge rst)
+begin
+        if(rst) begin
+		C[0] <= 0;
+		CD[0] <= 0;
+	end else begin 
+		C[0] <= I[2];
+		CD[0]<= C[0];
+		C[1] <= C[0] - CD[0];
+	end
+end
+
+generate
+	genvar i; 
+	for(i = 1; i < N; i = i + 1) begin: integrator 
+		wire [25:0] Itemp;
+		assign Itemp = I[i];//VISUALIZATION FOR GTKWAVE
+		always @(posedge clk or posedge rst) 
+		begin 
+		if(rst) begin
+			I[i][25:0] <= 0;
+		end else begin 
+			I[i][25:0] <= I[i][25:0] + I[i-1][25:0];
+		end
+	end
+end
+	for(i = 1; i < N; i = i + 1) begin: comb //COMBS
+		wire [25:0] Ctemp;
+		wire [25:0] CDtemp;
+		assign Ctemp = C[i];//VISUALIZATION FOR GTKWAVE
+		assign CDtemp = CD[i];//VISUALIZATION FOR GTKWAVE
+		always @(posedge clkdiv or posedge rst)
+		begin
+                if(rst) begin
+			C[i] <= 0;
+			CD[i] <= 0;
+		end else begin 
+			CD[i] <= C[i];
+			C[i+1] <= C[i] - CD[i];
+		end
+	end
+end
+endgenerate
+
+assign y_out = C[N][25:9];
+
+endmodule
---
+++ Gateware/tests/pdm.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import re
+
+def pdm(x):
+    n = len(x)
+    y = np.zeros(n)
+    error = np.zeros(n+1)
+    for i in range(n):
+        y[i] = 1 if x[i] >= error[i] else 0 
+        error[i+1] = y[i] - x[i] + error[i] 
+    return y, error[0:n]
+
+n = 3000
+fclk = 3.125e7 
+t = np.arange(n) / fclk 
+f_sin = 15000; #2khz
+
+
+x = 0.5 + 0.5 * np.sin(2*np.pi*f_sin*t)
+y, error = pdm(x) 
+
+
+plt.plot(1e9*t, x, label='input signal')
+plt.step(1e9*t, y, label='pdm signal',  linewidth=2.0)
+#plt.step(1e9*t, error, label='error')
+plt.xlabel('Time (ns)')
+plt.ylim(-0.05,1.05)
+plt.legend()
+plt.show()
+print(''.join(map(lambda x:str(int(x)),y)))
