77ce4067428be58cf91fb717724ada920c1fb667
cgbarn35 <cgbarn35@gmail.com>
2025-08-04 23:22:52 -0700

    created fifo testbench

--- Gateware/src/dsp/rtl/sync_fifo.v
+++ Gateware/src/dsp/rtl/sync_fifo.v
@@ -1,30 +1,36 @@
 `timescale 1ns / 1ps
 
-module sync_fifo #(parameter WIDTH = 18, DEPTH = 32)(
+module sync_fifo #(parameter AWIDTH = 18, BWIDTH = 25, DEPTH = 32)(
 	input clk,
 	input rst,
 	input wr_en,
 	input rd_en,
 	input reg [AWIDTH-1:0] a_in,
 	input reg [BWIDTH-1:0] b_in,
-	output reg [WIDTH-1:0] c_out,
+	output reg [AWIDTH-1:0] a_out,
+	output reg [BWIDTH-1:0] b_out,
 	output empty,
 	output full
 );
 
-
 reg [$clog2(DEPTH):0] wr_ptr;
 reg [$clog2(DEPTH):0] rd_ptr;
 
-reg [WIDTH-1:0] mem [0:DEPTH-1];
+reg [AWIDTH-1:0] aMEM [0:DEPTH-1];
+reg [BWIDTH-1:0] bMEM [0:DEPTH-1];
 
+initial begin 
+	wr_ptr <= 0;
+	rd_ptr <= 0;
+end
 
 always @(posedge clk) begin 
 	if(rst) begin 
 		wr_ptr <= 0;
 	end else begin 
 		if(wr_en & !full) begin 
-			fifo[wr_ptr] <= x_in;
+			aMEM[wr_ptr] <= a_in;
+			bMEM[wr_ptr] <= b_in;
 			wr_ptr <= wr_ptr + 1;
 		end
 	end
@@ -35,7 +41,8 @@ always @(posedge clk) begin
 		rd_ptr <= 0;
 	end else begin 
 		if(rd_en & !empty) begin
-			c_out <= fifo[rd_ptr];
+			a_out <= aMEM[rd_ptr];
+			b_out <= bMEM[rd_ptr];
                         rd_ptr <= rd_ptr + 1;
 		end
         end
---
+++ Gateware/src/dsp/tb/fir_fifo/fifo.sv
@@ -0,0 +1,93 @@
+`timescale 100ns / 10ps
+`define HalfClockPeriod 1.6275
+ 
+module fifoUUT;
+
+parameter AWIDTH = 18;
+parameter BWIDTH = 25;
+
+initial begin
+        $dumpfile("../../../../build/fifo.vcd");
+	$dumpvars;
+end
+
+reg stop;
+
+//INPUT
+reg CLK;
+reg RST;
+reg wr_en;
+reg rd_en;
+reg [AWIDTH-1:0] a_in;
+reg [BWIDTH-1:0] b_in;
+//OUTPUT
+reg [AWIDTH-1:0] a_out;
+reg [BWIDTH-1:0] b_out;
+reg empty;
+reg full;
+//FILE CONTENTS
+
+
+sync_fifo #(.AWIDTH(18), .BWIDTH(25), .DEPTH(32)) uutF (
+	.clk(CLK),
+	.rst(RST),
+	.wr_en(wr_en),
+	.rd_en(rd_en),
+	.a_in(a_in),
+	.b_in(b_in),
+	.a_out(a_out),
+	.b_out(b_out),
+	.empty(empty),
+	.full(full)
+        );
+
+initial begin
+	stop <= 1;
+        CLK <= 0;
+        RST <= 0;
+	wr_en <= 0;
+	rd_en <= 0;
+        #10 
+        RST <= 1;
+        #10 
+        RST <= 0;
+end
+
+
+initial begin 
+	@(posedge CLK);
+
+	for(int i = 0; i < 500; i = i+1) begin 
+		while(full) begin 
+			@(posedge CLK);
+			$display("%d Fifo Full",$time);
+		end
+	wr_en <= $random;
+	a_in <= $random;
+	b_in <= $random;
+	
+	@(posedge CLK);
+	end
+	stop <= 0;
+        $finish;
+end
+
+initial begin @(posedge CLK);
+
+	while(stop) begin 
+		while(empty) begin 
+		@(posedge CLK);
+		rd_en <= 0;
+		$display("%d FIFO empty",$time);
+		end
+	rd_en <= $random;
+	@(posedge CLK);
+	end 
+end
+
+//CLK INITIALIZATION
+always begin
+        #`HalfClockPeriod CLK = ~CLK;                         
+end
+
+endmodule
